a405e045523ee950ccf9e9beb00afd61e11f3d3a
Julian Jelfs <julian.jelfs@gmail.com>
2013-04-05 16:24:07 +0100

    got google login working. Think I might just about have had enough of geddy now. I can't get it to debug from web storm and that makes it very difficult to make progress. Not sure how much of this is because I'm working on windows.

--- app/helpers/passport/actions.js
+++ app/helpers/passport/actions.js
@@ -1,120 +1,139 @@
 var passport = require('passport')
-  , user = require('./user')
-  , config = geddy.config.passport
-  , successRedirect = config.successRedirect
-  , failureRedirect = config.failureredirect
-  , bcrypt = require('bcrypt')
-  , cryptPass;
+    , LocalStrategy = require('passport-local').Strategy
+    , user = require('./user')
+    , successRedirect = geddy.config.passport.successRedirect
+    , failureRedirect = geddy.config.passport.failureRedirect
+    , bcrypt = require('bcrypt')
+    , cryptPass;
 
 var SUPPORTED_SERVICES = [
-      'twitter'
-    , 'facebook'
-    , 'yammer'
-    ];
+    'twitter'
+    , 'google'
+];
+
+passport.use(new LocalStrategy(function (username, password, done) {
+    geddy.model.User.first({username: username}, function (err, user) {
+        if (err) {
+            done(err, null);
+        }
+        if (user) {
+            if (!cryptPass) {
+                cryptPass = require('./index').cryptPass;
+            }
+
+            if (bcrypt.compareSync(password, user.password)) {
+                done(null, user);
+            }
+            else {
+                done({message: 'Not found'}, null);
+            }
+        }
+        else {
+            done({message: 'Not found'}, null);
+        }
+    });
+}));
 
 SUPPORTED_SERVICES.forEach(function (item) {
-  var config = {
-        callbackURL: geddy.config.fullHostname + '/auth/' +
-            item + '/callback'
-      }
-    , Strategy = require('passport-' + item).Strategy;
+    var config = {
+            callbackURL: geddy.config.fullHostname + '/auth/' +
+                item + '/callback'
+        }
+        , Strategy = require('passport-' + item).Strategy
+        , verifyCallback = function (token, tokenSecret, profile, done) {
+            done(null, profile);
+        };
 
-  geddy.mixin(config, geddy.config.passport[item]);
-  passport.use(new Strategy(config,
-      function(token, tokenSecret, profile, done) {
-    done(null, profile);
-  }));
+    geddy.mixin(config, geddy.config.passport[item]);
+
+    if (item == 'google') {
+        verifyCallback = function (identity, profile, done) {
+            done(null, profile);
+        };
+    }
+    passport.use(new Strategy(config, verifyCallback));
 });
 
 var actions = new (function () {
-  var self = this;
+    var self = this;
 
-  var _createInit = function (authType) {
-        return function (req, resp, params) {
-          var self = this;
-          req.session = this.session.data;
-          // FIXME: hack until Passport defers to resp.redirect
-          resp.end = function () {};
-          resp.setHeader = function (headerName, val) {
-            resp.redirect(val);
-          };
-          passport.authenticate(authType)(req, resp);
-        };
-      }
+    var _createInit = function (authType) {
+            return function (req, resp, params) {
+                req.session = this.session.data;
+                // FIXME: hack until Passport defers to resp.redirect
+                resp.end = function () {
+                };
+                resp.setHeader = function (headerName, val) {
+                    resp.redirect(val);
+                };
+                passport.authenticate(authType)(req, resp);
+            };
+        }
 
-    , _createCallback = function (authType) {
-        return function (req, resp, params) {
-          var self = this;
-          req.session = this.session.data;
-          // FIXME: hack until Passport defers to resp.redirect
-          resp.end = function () {};
-          resp.setHeader = function (headerName, val) {
-            resp.redirect(val);
-          };
-          passport.authenticate(authType, function (err, profile) {
-            if (!profile) {
-              self.redirect(failureRedirect);
-            }
-            else {
-              try {
-                user.lookupByPassport(authType, profile, function (err, user) {
-                  if (err) {
-                    self.error(err);
-                  }
-                  else {
-                    self.session.set('userId', user.id);
-                    self.session.set('authType', authType);
-                    self.redirect(successRedirect);
-                  }
+        , _createCallback = function (authType) {
+            return function (req, resp) {
+                var self = this;
+                req.session = this.session.data;
+                // FIXME: hack until Passport defers to resp.redirect
+                resp.end = function () {
+                };
+                resp.setHeader = function (headerName, val) {
+                    resp.redirect(val);
+                };
+                passport.authenticate(authType, function (err, profile) {
+                    if (!profile) {
+                        self.redirect(failureRedirect);
+                    }
+                    else {
+                        try {
+                            user.lookupByPassport(authType, profile, function (err, user) {
+                                if (err) {
+                                    self.error(err);
+                                }
+                                else {
+                                    self.session.set('userId', user.id);
+                                    self.session.set('authType', authType);
+                                    self.redirect(successRedirect);
+                                }
+                            });
+                        }
+                        catch (e) {
+                            self.error(e);
+                        }
+                    }
+                })(req, resp, function (e) {
+                    if (e) {
+                        self.error(e);
+                    }
                 });
-              }
-              catch (e) {
-                self.error(e);
-              }
-            }
-          })(req, resp, function (e) {
-            if (e) {
-              self.error(e);
-            }
-          });
+            };
         };
-      };
 
-  this.local = function (req, resp, params) {
-    var self = this
-      , username = params.username
-      , password = params.password;
+    this.local = function (req, resp, params) {
+        var self = this
+            , handler = function (badCredsError, user, noCredsError) {
+                if (badCredsError || noCredsError) {
+                    self.redirect(failureRedirect);
+                }
+                else {
+                    self.session.set('userId', user.id);
+                    self.session.set('authType', 'local');
+                    self.redirect(successRedirect);
+                }
+            };
+        // FIXME: Passport wants a request body or query
+        req.body = {
+            username: params.username, password: params.password
+        };
+        passport.authenticate('local', function () {
+            handler.apply(null, arguments);
+        })(req, resp, handler);
+    };
 
-    geddy.model.User.first({username: username}, {nocase: ['username']},
-        function (err, user) {
-      var crypted;
-      if (err) {
-        self.redirect(failureRedirect);
-      }
-      if (user) {
-        if (!cryptPass) {
-          cryptPass = require('./index').cryptPass;
-        }
-
-        if (bcrypt.compareSync(password, user.password)) {
-          self.session.set('userId', user.id);
-          self.session.set('authType', 'local');
-          self.redirect(successRedirect);
-        }
-        else {
-          self.redirect(failureRedirect);
-        }
-      }
-      else {
-        self.redirect(failureRedirect);
-      }
+    SUPPORTED_SERVICES.forEach(function (item) {
+        self[item] = _createInit(item);
+        self[item + 'Callback'] = _createCallback(item);
     });
-  };
-
-  SUPPORTED_SERVICES.forEach(function (item) {
-    self[item] = _createInit(item);
-    self[item + 'Callback'] = _createCallback(item);
-  });
 
 })();
 
